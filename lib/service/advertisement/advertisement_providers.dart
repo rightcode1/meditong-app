f7bb6c6051e9da33eb8206cb689e3b87dfc8d1bd
Jihyun, Jeong <dinb1242@naver.com>
2024-04-28 20:25:31 +0900

    [fix] 광고 도메인 및 프로바이더 구성

---
+++ lib/service/advertisement/advertisement_providers.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mediport/core/exception/request_exception.dart';
+import 'package:mediport/domain/model/advertisement/res/advertisement_res_list.dart';
+import 'package:mediport/domain/repository/advertisement/advertisement_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'advertisement_providers.g.dart';
+
+@riverpod
+Future<List<AdvertisementResList>> advertisementList(AdvertisementListRef ref, { String? location, bool? active }) async {
+  try {
+    final response = await ref.read(advertisementRepositoryProvider).list(location: location, active: active);
+
+    return response.data!;
+  } catch (err, stack) {
+    debugPrint(err.toString());
+    debugPrint(stack.toString());
+
+    throw RequestException(message: err.toString());
+  }
+}
